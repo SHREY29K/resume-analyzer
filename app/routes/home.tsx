8e9d89d49a3242540075fb62d4491d2b9c7363e8
Shrey Kothari <shk29171112@gmail.com>
2025-08-14 08:10:23 +0530

    extra changes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,22 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Review" },
+    { name: "description", content: "Provides appropriate feedback for you to get hired!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+          <div className="page-heading">
+              <h1>Track your applications and resume ratings</h1>
+              <h2>Do all of this with AI-powered feedback.</h2>
+          </div>
+      </section>
+
+  </main>;
 }
